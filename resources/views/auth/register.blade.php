a02644dd61efa89c0565aaddd2ece44246dc1292
RomarioGiacholari <giacholari@gmail.com>
2020-04-06 13:02:23 +0100

    add plan input on register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('title', 'Register')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -50,6 +50,16 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="plan" class="col-md-4 col-form-label text-md-right">Plan</label>
+                            <div class="col-md-6">
+                                <select class="form-control" name="plan" id="plan">
+                                    <option value="Monthly">Monthly (£10)</option>
+                                    <option value="Yearly">Yearly (£90)</option>
+                                </select>
                             </div>
                         </div>
 
